8bb7a5ec112089568cd0350574e8b4371d869f01
Kevin Jolley <30558808+Serrowxd@users.noreply.github.com>
2018-04-04 15:33:38 -0500

    Updates

--- resume/src/index.js
+++ resume/src/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+import axios from 'axios';
+
+class List extends React.Component {
+  state = {
+    users: [],
+  };
+
+  render() {
+    return(
+      <ul>
+        {this.state.users.map(user =><li key={user.id}>{user.name}</li>)}
+      </ul>
+    );
+  }
+
+      componentDidMount() {
+        axios
+        .get('http://localhost:5000/api/users')
+        .then(response => {
+          this.setState({ users: response.data });
+        })
+        .catch(error => console.log(error));
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
